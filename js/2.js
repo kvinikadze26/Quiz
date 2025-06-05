604cbc3f0d014c0ca585f79b30830127f09997b8
kvinikadze26 <iraklikvinikadze123@gmail.com>
2025-06-05 17:32:09 +0400

    add homework

---
+++ js/2.js
@@ -0,0 +1,20 @@
+for (let i = 5; i <= 100; i++) {
+    console.log(i);
+}
+
+let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+for (let i = 0; i < array1.length; i++) {
+    if (array1[i] > 0 && array1[i] < 10) {
+        console.log(array1[i]);
+    }
+}
+
+let array2 = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < array2.length; i++) {
+    sum += array2[i];
+}
+console.log(sum);
+
